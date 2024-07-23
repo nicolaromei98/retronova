751fb231b7570aa6e61e75f3060fb7670534ff11
nicolaromei98 <167851712+nicolaromei98@users.noreply.github.com>
2024-07-23 13:46:53 +0200

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,1 +1,66 @@
-$(document).ready(function(){function t(t){gsap.set(".load_grid",{display:"grid"}),gsap.fromTo(".load_grid-item",{opacity:0},{opacity:1,duration:.001,stagger:{amount:.5,from:"random"},onComplete(){window.location.href=t}})}!function t(){let o=$("body").attr("data-collection"),a="#070707";switch(o){case"Space Age Glam":a="#FD8A46";break;case"Atomic Allure":a="#F3EDD8";break;case"Quantum Glamour":a="#A24EB5";break;case"Stellar Elegance":a="#008AA1"}$(".load_grid-item").css("background-color",a)}(),gsap.to(".load_grid-item",{opacity:0,duration:.001,stagger:{amount:.7,from:"random"},onComplete(){gsap.set(".load_grid",{display:"none"})}}),$("a").on("click",function(o){if($(this).prop("hostname")===window.location.host&&-1===$(this).attr("href").indexOf("#")&&"_blank"!==$(this).attr("target")){o.preventDefault();let a=$(this).attr("href");t(a)}}),$(document).on("click",".next_collection",function(o){o.preventDefault();let a=$(this).attr("href");gsap.to(".load_grid-item",{opacity:0,duration:.001,stagger:{amount:.5,from:"random"},onComplete(){t(a)}})}),window.onpageshow=function(t){(t.persisted||"back_forward"===performance.getEntriesByType("navigation")[0].type)&&window.location.reload()},$(window).on("load",function(){"back_forward"===performance.getEntriesByType("navigation")[0].type&&window.location.reload()})});
+$(document).ready(function() {
+    function animateOutAndRedirect(url) {
+        console.log('Starting animation out...');
+        gsap.set(".load_grid", { display: "grid" });
+        return gsap.to(".load_grid-item", {
+            opacity: 0,
+            duration: 0.5, // Aumentiamo la durata per vedere meglio l'animazione
+            stagger: { amount: 0.5, from: "random" }
+        }).then(() => {
+            console.log('Animation complete, redirecting...');
+            window.location.href = url;
+        });
+    }
+
+    function updateBackgroundColor() {
+        let collection = $("body").attr("data-collection");
+        let backgroundColor = "#070707";
+
+        switch (collection) {
+            case "Space Age Glam":
+                backgroundColor = "#FD8A46";
+                break;
+            case "Atomic Allure":
+                backgroundColor = "#F3EDD8";
+                break;
+            case "Quantum Glamour":
+                backgroundColor = "#A24EB5";
+                break;
+            case "Stellar Elegance":
+                backgroundColor = "#008AA1";
+                break;
+        }
+
+        $(".load_grid-item").css("background-color", backgroundColor);
+    }
+
+    updateBackgroundColor();
+
+    $("a").on("click", function(event) {
+        if ($(this).prop("hostname") === window.location.host && $(this).attr("href").indexOf("#") === -1 && $(this).attr("target") !== "_blank") {
+            event.preventDefault();
+            let href = $(this).attr("href");
+            console.log('Link clicked, starting animation for: ', href);
+            animateOutAndRedirect(href);
+        }
+    });
+
+    $(document).on("click", ".next_collection", function(event) {
+        event.preventDefault();
+        let href = $(this).attr("href");
+        console.log('Next collection clicked, starting animation for: ', href);
+        animateOutAndRedirect(href);
+    });
+
+    window.onpageshow = function(event) {
+        if (event.persisted || performance.getEntriesByType("navigation")[0].type === "back_forward") {
+            window.location.reload();
+        }
+    };
+
+    $(window).on("load", function() {
+        if (performance.getEntriesByType("navigation")[0].type === "back_forward") {
+            window.location.reload();
+        }
+    });
+});
